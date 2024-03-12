f844f263d4e0f4d0bbef3793748982ebb83e4a2e
tommasorazza <tommaso.razza@mail.polimi.it>
2024-03-12 15:06:44 +0100

    aaa

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -3,9 +3,12 @@ package it.polimi.ingsw;
 /**
  * Hello world!
  *
+ *
  */
+
 public class App 
 {
+    int a;
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
