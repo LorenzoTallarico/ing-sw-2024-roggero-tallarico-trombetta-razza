bb4af2cc4f691fc3f882149ad033cb32aa3f8d51
tommasorazza <tommaso.razza@mail.polimi.it>
2024-03-13 14:07:56 +0100

    ho sistemato reqResource in goldCard (è una mappa non piu un vettore)

--- src/main/java/it/polimi/ingsw/model/GoldCard.java
+++ src/main/java/it/polimi/ingsw/model/GoldCard.java
@@ -1,10 +1,12 @@
 package it.polimi.ingsw.model;
+import  java.util.Map;
+import  java.util.HashMap;
 
 public class GoldCard extends Card {
-    private int[] reqResources = new int[4];
+    private Map<Resource, Integer> reqResources = new HashMap<>();
     private Item reqItem;
     private ReqPoint reqPoints;
-    public GoldCard(int points, Resource resource, Corner[] frontCorners, Corner[] backCorners,int[] reqResources, Item reqItem, ReqPoint reqPoints) {
+    public GoldCard(int points, Resource resource, Corner[] frontCorners, Corner[] backCorners, Map<Resource,Integer> reqResources, Item reqItem, ReqPoint reqPoints) {
         this.points = points;
         this.resource = resource;
         for(int i = 0; i < frontCorners.length; i++) {
@@ -13,37 +15,18 @@ public class GoldCard extends Card {
         for(int i = 0; i < backCorners.length; i++) {
             this.backCorners[i] = backCorners[i];
         }
-        for(int i = 0; i < reqResources.length; i++) {
-            this.reqResources[i] = reqResources[i];
+        for(int i = 0; i < reqResources.size(); i++) {
+            reqResources.put(resource.WOLF,0);
+            reqResources.put(resource.BUTTERFLY,0);
+            reqResources.put(resource.LEAF,0);
+            reqResources.put(resource.MUSHROOM,0);
         }
         this.reqItem = reqItem;
         this.reqPoints = reqPoints;
     }
 
-    /*
-    REMAKE
-    va fatta la classe con una mappa e non un vettore, rifare il countResource dopo
-     */
     public int countResource(Resource res){
-        int rit = 0;
-        switch(res) {
-            case WOLF:
-                rit = reqResources[0];
-                break;
-            case BUTTERFLY:
-                rit = reqResources[1];
-                break;
-            case LEAF:
-                rit = reqResources[2];
-                break;
-            case MUSHROOM:
-                rit = reqResources[3];
-                break;
-            default:
-                rit = -1;
-                break;
-        }
-        return rit;
+        return reqResources.get(res);
     }
     public ReqPoint getPointsType() {
         return reqPoints;
